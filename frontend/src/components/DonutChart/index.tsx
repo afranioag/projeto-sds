db8e75226bc05449106d35edf67df2cc5c399826
afranio.garcia <afranio.cet@gmail.com>
2021-11-06 01:30:41 -0300

    Barchart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -20,14 +20,9 @@ const DonutChart = () => {
         const data = response.data as SaleSum[];
         const myLabels = data.map((x) => x.saleName);
         const mySeries = data.map((x) => x.sum);
-        setChartData({ labels: myLabels, series: mySeries });
+        setChartData({ labels: myLabels, series: mySeries }); 
       });
   }, []);
-
-  // const mockData = {
-  //     series: [477138, 499928, 444867, 220426, 473088],
-  //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-  // }
 
   const options = {
     legend: {
